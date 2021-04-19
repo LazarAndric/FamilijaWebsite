2fe8548445917a8cf2dc41e2026196f1c702d482
Lazar Andric <Lazarandric97@gmail.com>
2021-04-19 18:00:10 +0200

    Add amazon lambda dependency, and add json aws config

--- FamilijaApi/DTOs/UserReadDTOs.cs
+++ FamilijaApi/DTOs/UserReadDTOs.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace FamilijaApi.DTOs
 {
     public class UserReadDTO
--- FamilijaApi/Data/SqlUserRepo.cs
+++ FamilijaApi/Data/SqlUserRepo.cs
@@ -3,6 +3,7 @@ using FamilijaApi.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace FamilijaApi.Data
 {
